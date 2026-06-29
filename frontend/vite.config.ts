6752ea2da39076987ce37a33ee6a85b9afba06ae
Aman Kasyap <aman.kasyap@suretekinfosoft.com>
2026-06-29 08:32:40 -0700

    fixed vite error for production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from "path";
-// https://vitejs.dev/config/
+import path from 'path';
+
 export default defineConfig({
   plugins: [react()],
-    resolve: {
+
+  resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['qckpi.city.financial'],
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['qckpi.city.financial'],
   },
 });
